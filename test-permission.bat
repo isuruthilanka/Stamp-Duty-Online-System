9a00cf45d21a347611df158a3361e68b293e8597
Lahiru Munasinghe <lahirumm92@gmail.com>
2026-03-09 18:40:35 +0530

    auto: changes at 2026-03-09 18:40:35

--- test-permission.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Permission Test > test_file.txt
-echo [OK] If you see this, permissions are fine.
-pause
--- test-server.cjs
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.createServer((req, res) => {
-    res.end('Server is up!');
-}).listen(5174, '127.0.0.1', () => {
-    console.log('Test server running at http://localhost:5174/');
-});
