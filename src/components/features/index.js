1a492af1f8fd684dbe88dd89c16b73a7787b0f96
Lahiru Munasinghe <lahirumm92@gmail.com>
2026-03-09 18:47:12 +0530

    auto: changes at 2026-03-09 18:47:12

---
+++ src/components/features/index.js
@@ -0,0 +1,8 @@
+export { default as AdminPanel } from './AdminPanel';
+export { default as ApplicationDetail } from './ApplicationDetail';
+export { default as CommonDashboard } from './CommonDashboard';
+export { default as DeficiencyCalculator } from './DeficiencyCalculator';
+export { default as OfficialDeficiencyNotice } from './OfficialDeficiencyNotice';
+export { default as OfficialOpinionForm } from './OfficialOpinionForm';
+export { default as StampDutyCalculator } from './StampDutyCalculator';
+export { default as StampDutyRegistration } from './StampDutyRegistration';
