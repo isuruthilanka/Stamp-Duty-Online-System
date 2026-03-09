46790167536fb5535f421602aa9f628b6f309737
Lahiru Munasinghe <lahirumm92@gmail.com>
2026-03-09 17:44:46 +0530

    fix: replace wildcard * with /{*path} for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -496,7 +496,7 @@ app.use((err, req, res, next) => {
 if (process.env.NODE_ENV === 'production') {
     const distPath = path.join(__dirname, '..', 'dist');
     app.use(express.static(distPath));
-    app.get('*', (req, res) => {
+    app.get('/{*path}', (req, res) => {
         res.sendFile(path.join(distPath, 'index.html'));
     });
 }
